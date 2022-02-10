025aa1d27eb8fab8dddcc885ad5a69353314c68e
slilver520 <slilver520@naver.com>
2022-02-10 12:35:48 +0900

    배열 렌더링하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,37 +1,42 @@
 import React from 'react';
 
 function User({user}){
-    <div>
+    return (
         <div>
             <b>{user.username}</b>
             <span>({user.email})</span>
         </div>
-    </div>
+    );
 
-console.log({user})
 }
 
 function UserList(){
-    // const users = [
-    //     {
-    //         id: 1,
-    //         username: 'yh',
-    //         email:'yh@gmail.com'
-    //     },
-    //     {
-    //         id: 2,
-    //         username:'test',
-    //         email:'test@test.com'
-    //     },
-    //     {
-    //         id: 3,
-    //         username:'ex',
-    //         email:'ex@example.com'
-    //     }
-    // ]
+    const users = [
+        {
+            id: 1,
+            username: 'yh',
+            email:'yh@gmail.com'
+        },
+        {
+            id: 2,
+            username:'test',
+            email:'test@test.com'
+        },
+        {
+            id: 3,
+            username:'ex',
+            email:'ex@example.com'
+        }
+    ]
 
     return(
-        <User/>
+        <div>
+            {
+                users.map(
+                    user => (<User user={user} key={user.id}/>)
+                )
+            }
+        </div>
     )
 }
 
