558e1dc5a22af1f24477be0757ad216b46cd7ef5
slilver520 <slilver520@naver.com>
2022-02-14 21:57:26 +0900

    useEffect를 사용하여 마운트 업데이트 시 할 작업 설정하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function User({user, onRemove, onToggle}){
     const {username, email, id, active} = user;
+
+    useEffect(() => {
+        console.log('컴포넌트가 화면에 나타남(==마운트)')
+
+        //컴포넌트가 사라질때 특정작업을 하는 방법 : 그냥 반환해준다.
+        return () =>{
+            console.log('컴포넌트가 화면에서 사라짐(==언마운트)')
+        }
+    }, []);
+    //[]가 비어있으면 처음 화면에 나타날때만 호출됨
+
     return (
         <div>
             <b
