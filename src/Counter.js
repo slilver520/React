5f492a95b97f353e41cb4b0737cb7fd7ddd6e079
slilver520 <slilver520@naver.com>
2022-02-16 01:01:06 +0900

    useReducer 기초

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,48 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+//useReducer라는 Hook 함수를 사용하면 컴포넌트의 상태 업데이트 로직을 컴포넌트에서 분리시키는 것이 가능하다. 심지어 다른 파일에 작성 후 불러올 수도 있다.
+//( reducer란 상태를 업데이트하는 함수를 의미)
+
+//useState와 차이점
+//useState는 설정하고 싶은 다음 상태를 직접 지정해주는 방식으로 상태를 업데이트해주는 반면에
+//useReducer는 'action'이라는 객체(업데이트할 때 참조하는 객체)를 기반으로 상태를 업데이트한다.
+
+function reducer(state, action){
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            throw new Error('Unhandle action');
+    }
+}
 
 function Counter(){
-    const [num, setNum] = useState(0);
+    // const [num, setNum] = useState(0);
+    const [number, dispatch] = useReducer(reducer, 0);
+    //dispatch: action을 발생시킨다.
+    //useReducer(함수, 기본값)
     const style ={
         margin:5
     }
     const onIncrease = () =>{
-        setNum(n => n + 1);
+        //setNum(n => n + 1);
         // n = prevNumber (함수형 업데이트 for 성능 최적화)
+
+        // ㄴ useReducer하기 위해 수정됨
+        dispatch({
+            type:'INCREMENT',
+        })
     }
     const onDecrease = () => {
-        setNum(prevNumber => prevNumber - 1);
+        //setNum(prevNumber => prevNumber - 1);
+        dispatch({
+            type:'DECREMENT',
+        })
     }
     return (
         <div>
-            <h1>{num}</h1>
+            <h1>{number}</h1>
             <button 
                 className='counterBtn'
                 onClick={onIncrease}
--- src/CreateUser.js
+++ src/CreateUser.js
@@ -21,5 +21,11 @@ function CreateUser({ username, email, onChange, onCreate}){
         </div>
     )
 }
-export default React.memo(CreateUser);
-//React.memo : props가 바꼈을때만 리렌더링 해줌
+export default React.memo(CreateUser,
+    (prevProps, nextProsp) => nextProsp.users === prevProps.users
+);
+//React.memo : props가 바꼈을때만 리렌더링 해줌 (for 최적화)
+
+//React.memo 함수를 사용할 때 두 번째 파라미터에 props are equal 함수를 넣어줄 수 있다.
+//prevProps와 nextProps가 같은지 확인을 해서 같다면 리 렌더링을 막고 다르면 리 렌더링을 하는 식의 함수이다.
+//이 함수는 잘못 사용할 경우 치명적 오류가 될 수 있기 때문에 주의해서  사용하도록 하자.
