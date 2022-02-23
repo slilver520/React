b19b098e32ce55aec6a93ade77cc8444bb1fafb6
slilver520 <slilver520@naver.com>
2022-02-23 15:38:47 +0900

    1.2 Router Setup

---
+++ src/components/Router.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
+//Switch -> Routes
+
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    return (
+        <Router>
+            <Routes>
+                {isLoggedIn ? (
+                        <>
+                            <Route 
+                                path={"/"} 
+                                element={ < Home />}
+                            >
+                            </Route>
+                        </> 
+                ) : (
+                    <Route 
+                        path={"/"} 
+                        element={ < Auth />}
+                    >
+                    </Route>
+                )}
+            </Routes>
+        </Router>
+    )
+}
+//로그인 (인증) 여부에 따라 달라지게함
+
+export default AppRouter;
---
+++ src/routes/Auth.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Auth = () => <span>Auth</span>
+export default Auth;
+//자동으로 import가 되도록 하고싶을때
