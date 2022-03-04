802a69ae15ee538040372993180c1ead6da50ba7
slilver520 <slilver520@naver.com>
2022-03-04 17:40:50 +0900

    #3.2 Getting the Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,32 @@
-import React, { useState} from "react";
+import React, { useEffect, useState} from "react";
 import {dbService} from 'fbase';
-import {addDoc, collection} from 'firebase/firestore';
+import {addDoc, collection, getDocs, query} from 'firebase/firestore';
 
 
 const Home = () => {
     const [nweet, setNweet] = useState('');
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const q = query(collection(dbService, "nweets"));
+        const querySnapshot = await getDocs(q);
+        
+        querySnapshot.forEach((doc) => {
+            const nweetObj = {
+                ...doc.data(),
+                id: doc.id,
+                }
+            setNweets(prev => [nweetObj, ...prev])
+            //새 트윗, 이전 트윗
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    },[]);
+
+
+
+
+
     const onSubmit = async(event) => {
         event.preventDefault();
         try{
@@ -23,6 +45,7 @@ const Home = () => {
         //event안에 있는 target안에 있는 value를 달라고하는 것
         setNweet(value)
     }
+console.log('nweets: ',nweets)
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -35,6 +58,13 @@ const Home = () => {
                 />
                 <input type="submit" value='Nweet'/>
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>
+                ))}
+            </div>
         </div>
     )
 };
